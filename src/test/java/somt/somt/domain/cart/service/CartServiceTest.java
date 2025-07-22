464cbcf8f39e4437eefd76f2c2395048c8cd3e12
janjan <97529177+zxc8661@users.noreply.github.com>
2025-07-22 15:47:59 +0900

    cart create test

--- src/test/java/somt/somt/domain/cart/service/CartServiceTest.java
+++ src/test/java/somt/somt/domain/cart/service/CartServiceTest.java
@@ -1,44 +1,112 @@
 package somt.somt.domain.cart.service;
 
-import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import somt.somt.common.exception.CustomException;
-import somt.somt.common.exception.ErrorCode;
 import somt.somt.common.security.dto.CustomUserData;
 import somt.somt.common.security.dto.CustomUserDetails;
 import somt.somt.domain.cart.dto.CartRequest;
-import somt.somt.domain.cart.dto.CartResponse;
 import somt.somt.domain.cart.entity.Cart;
 import somt.somt.domain.cart.repository.CartRepository;
-import somt.somt.domain.genre.entity.Genre;
-import somt.somt.domain.genreProduct.entity.GenreProduct;
 import somt.somt.domain.member.entity.Member;
-import somt.somt.domain.member.repository.MemberRepository;
 import somt.somt.domain.member.service.MemberService;
 import somt.somt.domain.product.entity.Product;
-import somt.somt.domain.product.repository.ProductRepository;
 import somt.somt.domain.product.service.ProductService;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static reactor.core.publisher.Mono.when;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
-@SpringBootTest
-@Transactional
+@ExtendWith(MockitoExtension.class)
 class CartServiceTest {
+
+    @Mock
+    CartRepository cartRepository;
+    @Mock
+    ProductService productService;
+    @Mock
+    MemberService memberService;
+    @InjectMocks
+    CartService cartService;
+
+    @Test
+    public void create_true(){
+        //given
+        CustomUserDetails mockCustomUserDetails =
+                new CustomUserDetails(
+                        new CustomUserData(
+                                1L,
+                                "member",
+                                "ADMIN",
+                                "",
+                                "nickname")
+                );
+        CartRequest cartRequest  = new CartRequest(10L,10);
+        Member mockMember = new Member(mockCustomUserDetails);
+        Product mockProduct = new Product(10L,"mock",new BigDecimal("100"),10,"10");
+
+        Cart mockCart =new Cart(10L,mockMember,mockProduct,10, LocalDateTime.now());
+
+
+
+        when(productService.getProduct(anyLong())).thenReturn(mockProduct);
+        when(memberService.getMember(anyLong())).thenReturn(mockMember);
+        when(cartRepository.existsByMemberIdAndProductId(mockMember.getId(),mockProduct.getId())).thenReturn(true);
+        when(cartRepository.findByMemberIdAndProductId(mockMember.getId(),mockProduct.getId())).thenReturn(mockCart);
+
+        //when
+        Long result = cartService.create(mockCustomUserDetails,cartRequest);
+        //then
+        verify(cartRepository).existsByMemberIdAndProductId(mockMember.getId(),mockProduct.getId());
+        verify(cartRepository).findByMemberIdAndProductId(mockMember.getId(),mockProduct.getId());
+
+    }
+
+    @Test
+    public void create_false(){
+        //given
+        CustomUserDetails mockCustomUserDetails =
+                new CustomUserDetails(
+                        new CustomUserData(
+                                1L,
+                                "member",
+                                "ADMIN",
+                                "",
+                                "nickname")
+                );
+        CartRequest cartRequest  = new CartRequest(10L,10);
+        Member mockMember = new Member(mockCustomUserDetails);
+        Product mockProduct = new Product(10L,"mock",new BigDecimal("100"),10,"10");
+
+        Cart mockCart =new Cart(10L,mockMember,mockProduct,10, LocalDateTime.now());
+
+
+
+        when(productService.getProduct(anyLong())).thenReturn(mockProduct);
+        when(memberService.getMember(anyLong())).thenReturn(mockMember);
+        when(cartRepository.existsByMemberIdAndProductId(mockMember.getId(),mockProduct.getId())).thenReturn(false);
+        when(cartRepository.save(any())).thenReturn(mockCart);
+        //when
+        Long result = cartService.create(mockCustomUserDetails,cartRequest);
+
+
+
+        //then
+        assertThat(result).isEqualTo(10L);
+        verify(cartRepository).save(any());
+
+
+    }
+
+
+
 
 }
